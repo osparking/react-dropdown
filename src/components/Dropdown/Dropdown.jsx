9da70e6b6ddfaba03e43ac7068ccc3989511ca54
osparking <jbpark03@gmail.com>
2024-11-25 20:13:29 +0900

    make DrowdownButton reveal param inside

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import DropdownButton from "../DropdownButton/DropdownButton";
 import DropdownContent from "../DropdownContent/DropdownContent";
 
-const Dropdown = (buttonText, content) => {
+const Dropdown = ({ buttonText, content }) => {
   return (
     <div>
       <DropdownButton>{buttonText}</DropdownButton>
