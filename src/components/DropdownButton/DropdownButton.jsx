a355b3ae0777c65e18af3040658050211ccd9b49
osparking <jbpark03@gmail.com>
2024-11-25 20:28:10 +0900

    https://react-icons.github.io/react-icons/

--- src/components/DropdownButton/DropdownButton.jsx
+++ src/components/DropdownButton/DropdownButton.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { SlArrowDown } from "react-icons/sl";
 
-const DropdownButton = ({children}) => {
-  return <div>{children}</div>;
+const DropdownButton = ({ children }) => {
+  return (
+    <div>
+      {children}
+      <span>
+        <SlArrowDown />
+      </span>
+    </div>
+  );
 };
 
 export default DropdownButton;
