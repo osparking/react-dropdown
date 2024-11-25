8ddfedba94f7bd305d804c6fc6b454f1278584c4
osparking <jbpark03@gmail.com>
2024-11-25 19:59:40 +0900

    Dropdown compo is mentioned in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
+import Dropdown from "./components/Dropdown/Dropdown";
 
 function App() {
   const items = [1, 2, 3, 4];
 
   return (
     <div className="App">
-      <div className="content">내려펼침</div>
+      <div className="content">
+        <Dropdown />
+      </div>
     </div>
   );
 }
--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Dropdown = () => {
   return (
     <div>
-      
+      드롭다운!
     </div>
   )
 }
