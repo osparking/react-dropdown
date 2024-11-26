0e0a42e008106a121b5cbaa94ba4f2a6a089a880
osparking <jbpark03@gmail.com>
2024-11-26 14:27:02 +0900

    remove dropdown list when outside clicked

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,15 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import DropdownButton from "../DropdownButton/DropdownButton";
 import DropdownContent from "../DropdownContent/DropdownContent";
 import "./Dropdown.css";
 
 const Dropdown = ({ buttonText, content }) => {
   const [open, setOpen] = useState(false);
+  const dropdownRef = useRef();
   const toggleDropdown = () => {
     setOpen(!open);
   };
+  useEffect(() => {
+    const handler = (event) => {
+      console.log("cal 됨");
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("click", handler);
+
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+  }, []);
   return (
-    <div className="dropdown">
+    <div className="dropdown" ref={dropdownRef}>
       <DropdownButton toggle={toggleDropdown} open={open}>
         {buttonText}
       </DropdownButton>
